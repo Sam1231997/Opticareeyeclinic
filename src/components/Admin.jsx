4c813deb89f49393b81c4a17aaf3b2cf58b2f06a
Sam1231997 <161332692+Sam1231997@users.noreply.github.com>
2025-01-25 10:20:04 -1200

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -11,7 +11,7 @@ const Admin = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://optibackend-1.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
